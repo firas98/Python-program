c29d13b536cd3dea1d743011b10cde4f0a6cc5f0
Syed Shakeeb Assil <30729905+assil97@users.noreply.github.com>
2018-10-14 17:32:31 +0530

    Add files via upload

---
+++ subtract.py
@@ -0,0 +1,8 @@
+#this is my subtract program
+#taking input a and b from the user
+a=input("Please enter the number a\n");
+b=input("Please enter the number b\n")
+#subtract from the two given numbers
+c=float(a)-float(b)
+#print the c
+print(c)
