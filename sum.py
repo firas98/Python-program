f71cc17ee3888f29653dd7ad9e4579b30f73924c
Syed Shakeeb Assil <30729905+assil97@users.noreply.github.com>
2018-10-14 17:32:47 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+#add two numbers 
+#input a from user and input b from user
+a=input("Please enter the number a\n")
+b=input("Please enter the number b\n")
+#assigning c for addition of a and b
+c = int(a) + int(b)
+#print the number
+print(c)
+
